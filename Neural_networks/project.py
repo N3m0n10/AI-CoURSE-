21087f6f23da3beb569c12a8adb6f5fce3d3484a
Nemo <139658039+N3m0n10@users.noreply.github.com>
2025-04-25 12:11:16 -0300

    update_grad_descent
    grad_desc
    save_weights

--- Neural_networks/project.py
+++ Neural_networks/project.py
@@ -20,9 +20,9 @@ import os
 # cross validation
 
 init_epsilon = 1e-2
-learning_rate = 0.0001
-epochs = 10000
-lbd = 0.00001
+learning_rate = 0.1
+epochs = 1000
+lbd = 0
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(current_dir, "classification2.txt")
@@ -85,7 +85,7 @@ else:
     print("gradient_check_failed")
 #raise ValueError("Gradient check done")
 
-#"""
+"""
 result = optimize.minimize(fun=J, jac=gradient_for_op_minimize,\
                 x0=initial_theta, method='TNC', args=(X, Y), options={'maxiter': 50000, 'disp': True})
 print(result)
@@ -102,11 +102,12 @@ reshaped_W = [
     result.x[split_points[i]:split_points[i+1]].reshape(shapes[i])
     for i in range(len(shapes))
 ]
-#"""
 """
+#"""
+
 result = gradient_descent(X, Y, W, learning_rate=learning_rate, epochs=epochs, lbd=lbd)
 reshaped_W = result
-"""
+#"""
 acc = 0
 for i in range(len(X)):
     acc += Y[i] - np.round(forward_prop(X[i], reshaped_W, n_layers)[0][-1])
@@ -127,7 +128,7 @@ if save.lower() == "y":
         f.truncate()  # Clear the file
         weights = f"Final weights: {reshaped_W}\n"
         f.write(old + weights)
-    
+
 """
 ## test 1 ->  
 result, H = gradient_descent(X, Y, W, learning_rate=0.01, epochs=300, lbd=0)
